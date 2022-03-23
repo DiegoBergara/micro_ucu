5ed44228c727dcdbfc6f7f8e7977f504f4de23d4
diegobergara <diegobj1998@gmail.com>
2022-03-23 19:26:30 -0300

    declared functions for each part

--- Lab01.X/main.asm
+++ Lab01.X/main.asm
@@ -1,20 +1,20 @@
 #define __SFR_OFFSET 0
-#define TURNLED 0
 #include "avr/io.h"
 .global main
 main:
-   
+setOutputs:
     sbi DDRB, 5
-    cbi PORTB, 5
     sbi DDRB, 4
-    cbi PORTB, 4
     sbi DDRB, 3
-    cbi PORTB, 3
     sbi DDRB, 2
+setLedsOn:
+    cbi PORTB, 5
+    cbi PORTB, 4
+    cbi PORTB, 3
     cbi PORTB, 2
-    
+turnLedsOff:
     sbi PORTB, 5
     sbi PORTB, 4
     sbi PORTB, 3
     sbi PORTB, 2
-    rjmp main
+    rjmp setLedsOn
