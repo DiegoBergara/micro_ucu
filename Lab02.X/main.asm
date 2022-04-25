cd4ded5ca92bf9561051559e09147b8552e789e0
diegobergara <diegobj1998@gmail.com>
2022-04-25 19:02:33 -0300

    lab02 finished with prettier delay

--- Lab02.X/main.asm
+++ Lab02.X/main.asm
@@ -8,28 +8,15 @@ SetUp:
     
 TurnLedsOn:
     cbi PORTB, 5 ; 1 cycle
-    
-DelayA: ; ((((4n-2)*m+(5m-2))*l)+5l-2) + 1
-ldi r18, 100
-LoopA0:
-    ldi r19, 99
-    LoopA1:
-	ldi r20, 100
-	LoopA2:
-	    dec r20
-	    cpse r20, r17
-	    rjmp LoopA2
-	dec r19
-	cpse r19, r17
-	rjmp LoopA1
-    dec r18
-    cpse r18, r17
-    rjmp LoopA0
+    call Delay
  
 TurnLedsOff:
     sbi PORTB, 5 ; 1 cycle
- 
-Delay2: ; ((((4n-2)*m+(5m-2))*l)+5l-2) + 1
+    call Delay
+    
+rjmp TurnLedsOn ; 2 cycle
+
+Delay: ; ((((4n-2)*m+(5m-2))*l)+5l-2) + 1
 ldi r18, 100 ; 1 cycle
 LoopB0: ; (((4n-2)*m+(5m-2))*l)+5l-2
     ldi r19, 99 ; 1 cycle
@@ -45,5 +32,4 @@ LoopB0: ; (((4n-2)*m+(5m-2))*l)+5l-2
     dec r18 ; 1 cycle
     cpse r18, r17 ; 1 cycle
     rjmp LoopB0 ; 2 cycle
-    
-rjmp TurnLedsOn ; 2 cycle
+ret
