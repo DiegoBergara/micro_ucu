8a82a04561e2420370405347df455312d7ec57e8
diegobergara <diegobj1998@gmail.com>
2022-04-25 20:36:27 -0300

    lab02 extended version

--- Lab02.X/main.asm
+++ Lab02.X/main.asm
@@ -3,16 +3,47 @@
 .global main
 main:
 SetUp:
-    sbi DDRB, 5 ; 1 cycle
-    ldi r17, 0 ; 1 cycle
+    sbi DDRB, 5
+    sbi DDRB, 4
+    sbi DDRB, 3
+    sbi DDRB, 2
+    ;sbi DDRD, 3
+    ldi r17, 0
     
 TurnLedsOn:
-    cbi PORTB, 5 ; 1 cycle
+    cbi PORTB, 5 
+    ;cbi PORTD, 3
     call Delay
  
 TurnLedsOff:
-    sbi PORTB, 5 ; 1 cycle
+    sbi PORTB, 5 
+    ;sbi PORTD, 3 
     call Delay
+   
+cbi PORTB, 4
+call Delay
+sbi PORTB, 4
+call Delay
+
+cbi PORTB, 3
+call Delay
+sbi PORTB, 3
+call Delay
+    
+cbi PORTB, 2
+call Delay
+sbi PORTB, 2
+call Delay
+
+cbi PORTB, 3
+call Delay
+sbi PORTB, 3
+call Delay
+    
+cbi PORTB, 4
+call Delay
+sbi PORTB, 4
+call Delay
     
 rjmp TurnLedsOn ; 2 cycle
 
